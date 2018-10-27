cf2abac28db30d6ab6cc60f58d9bb921c918b6ad
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-28 02:26:10 +0900

    solve raniy_days

--- problem/number_of_rainy_days.py
+++ problem/number_of_rainy_days.py
@@ -1,2 +1,15 @@
-import pandas
+import pandas as pd
 import pandasql
+
+def num_rainy_days(filename):
+    weather_data = pd.read_csv(filename)
+
+    q = """
+    SELECT sum(rain)
+    FROM weather_data
+    """
+
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days
+
+print(num_rainy_days('../datas/weather_underground.csv'))
