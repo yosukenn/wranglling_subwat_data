f71cd3004ec459c809e95db1ebd7193955d74204
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-28 13:52:17 +0900

    wrong?

--- problem/mean_temp_on_weelends.py
+++ problem/mean_temp_on_weelends.py
@@ -1,2 +1,20 @@
 import pandas as pd
 import pandasql
+
+def avg_weekend_temperature(filename):
+    weather_data = pd.read_csv(filename)
+
+    q = """
+    SELECT
+    cast(strftime('%W', date) as integer), avg(meantempi)
+    FROM
+    weather_data
+    WHERE
+    cast(strftime('%W', date) as integer) = 0 OR cast(strftime('%W', date) as integer) = 6
+    GROUP BY cast(strftime('%W', date) as integer)
+    """
+
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends
+
+print(avg_weekend_temperature('../datas/weather_underground.csv'))
