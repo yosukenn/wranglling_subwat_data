2419ca352b2a94b322e66f291d5c36705e07a0ff
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-28 13:58:26 +0900

    success

--- problem/mean_temp_on_weelends.py
+++ problem/mean_temp_on_weelends.py
@@ -6,12 +6,11 @@ def avg_weekend_temperature(filename):
 
     q = """
     SELECT
-    cast(strftime('%W', date) as integer), avg(meantempi)
+    avg(meantempi)
     FROM
     weather_data
     WHERE
     cast(strftime('%W', date) as integer) = 0 OR cast(strftime('%W', date) as integer) = 6
-    GROUP BY cast(strftime('%W', date) as integer)
     """
 
     mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
