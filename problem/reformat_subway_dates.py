a81587d263f369a3c40e11dd9e00fedf16fa8e7f
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-31 02:33:53 +0900

    done

--- problem/reformat_subway_dates.py
+++ problem/reformat_subway_dates.py
@@ -1,1 +1,7 @@
 import datetime
+
+def reformat_subway_dates(data):
+    # month-day-year -> year-month-day
+
+    date_formatted = datetime.datetime.strptime(date, '%m-%d-%y').date()
+    return date_formatted
